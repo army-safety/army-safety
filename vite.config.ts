f5cab50fcde7eec9efa815b3f72059ee04f1f317
zoma00 <zoma0097@gmail.com>
2025-11-17 14:32:42 +0200

    Modified these files for the new website name SECHIVE modified:   package-lock.json         modified:   package.json         modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/army-safety/',
+  base: command === 'serve' ? '/' : '/SECHIVE/',
   build: {
     outDir: 'dist',
     sourcemap: false,
