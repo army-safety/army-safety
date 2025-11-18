2095f3425cef4b4d47a807e9018fe4e6ed21cb66
zoma00 <zoma0097@gmail.com>
2025-11-19 01:36:52 +0200

    modified:   package.json         modified:   vite.config.ts for correct npm preview and production

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/SECHIVE/',
+  // Use '/' for dev and preview, '/SECHIVE/' for production build
+  base: mode === 'production' ? '/SECHIVE/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
